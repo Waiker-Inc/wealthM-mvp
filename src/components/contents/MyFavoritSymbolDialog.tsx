d535fecad542e47cea40632b1d5eb6c6a8d29ede
noahkang <yongsuek.kang@waiker.ai>
2025-06-25 16:59:33 +0900

    feat: 관심 심볼 선택 모달 ui 구현

--- src/components/contents/MyFavoritSymbolDialog.tsx
+++ src/components/contents/MyFavoritSymbolDialog.tsx
@@ -1,7 +1,9 @@
+import { Button } from '../ui/button';
 import { Dialog, DialogContent } from '../ui/dialog';
 import { Input } from '../ui/input';
 import Typography from '../ui/typography';
 import MarketTabs from './MarketTabs';
+import { GripVertical, X } from 'lucide-react';
 
 export default function MyFavoritSymbolDialog({
   isOpen,
@@ -28,7 +30,37 @@ export default function MyFavoritSymbolDialog({
             />
             <MarketTabs />
           </div>
-          <div className="bg-ground2 p-[40px_24px] w-[320px] h-full">123</div>
+          <div className="bg-ground2 p-[40px_24px] w-[320px] h-full relative">
+            <Typography size="body-lg">내 관심 심볼</Typography>
+            <ul className="mt-[24px]">
+              <li className="flex items-center gap-[12px] p-[8px] rounded-[8px] hover:bg-mono50 cursor-pointer">
+                <GripVertical className="text-mono200" size={16} />
+                <Typography>팔란티어</Typography>
+                <X className="text-mono200 ml-auto" size={16} />
+              </li>
+              <li className="flex items-center gap-[12px] p-[8px] rounded-[8px] hover:bg-mono50 cursor-pointer">
+                <GripVertical className="text-mono200" size={16} />
+                <Typography>팔란티어</Typography>
+                <X className="text-mono200 ml-auto" size={16} />
+              </li>
+              <li className="flex items-center gap-[12px] p-[8px] rounded-[8px] hover:bg-mono50 cursor-pointer">
+                <GripVertical className="text-mono200" size={16} />
+                <Typography>팔란티어</Typography>
+                <X className="text-mono200 ml-auto" size={16} />
+              </li>
+            </ul>
+            <div className="absolute bottom-[40px] left-0 right-0 px-[24px] flex items-end gap-[10px]">
+              <Button
+                className="flex-1 h-[60px] bg-transparent border border-mono200 text-white"
+                onClick={onClose}
+              >
+                <Typography>취소하기</Typography>
+              </Button>
+              <Button className="flex-1 h-[60px] bg-green700">
+                <Typography>완료하기</Typography>
+              </Button>
+            </div>
+          </div>
         </div>
       </DialogContent>
     </Dialog>
