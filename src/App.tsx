39c4298ca65ea26b1cc02af448259d8c31272c67
noahkang <yongsuek.kang@waiker.ai>
2025-06-25 21:52:19 +0900

    feat: tanstack query 초기 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,37 @@ import {
 import LeftPanel from './components/layout/LeftPanel';
 import Contents from './components/layout/Contents';
 import RightPanel from './components/layout/RightPanel';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // 기본 설정들
+      staleTime: 5 * 60 * 1000, // 5분
+      gcTime: 10 * 60 * 1000, // 10분 (이전 cacheTime)
+      retry: 1,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 function App() {
   return (
-    <ResizablePanelGroup direction="horizontal">
-      <ResizablePanel defaultSize={20}>
-        <LeftPanel />
-      </ResizablePanel>
-      <ResizableHandle />
-      <ResizablePanel>
-        <Contents />
-      </ResizablePanel>
-      <ResizableHandle />
-      <ResizablePanel defaultSize={20}>
-        <RightPanel />
-      </ResizablePanel>
-    </ResizablePanelGroup>
+    <QueryClientProvider client={queryClient}>
+      <ResizablePanelGroup direction="horizontal">
+        <ResizablePanel defaultSize={20}>
+          <LeftPanel />
+        </ResizablePanel>
+        <ResizableHandle />
+        <ResizablePanel>
+          <Contents />
+        </ResizablePanel>
+        <ResizableHandle />
+        <ResizablePanel defaultSize={20}>
+          <RightPanel />
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </QueryClientProvider>
   );
 }
 
