4f4c3e41cb3d47f552b1b58a1db47347ead426e1
HY.KIM <193584746+jjunk93@users.noreply.github.com>
2025-07-01 00:15:07 +0900

    feat : bolt logo

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,35 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.tsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import logo from "./assets/BOLT_LOGO.png";
 
 const setDarkMode = () => {
   if (
-    localStorage.theme === 'dark' ||
-    (!('theme' in localStorage) &&
-      window.matchMedia('(prefers-color-scheme: dark)').matches)
+    localStorage.theme === "dark" ||
+    (!("theme" in localStorage) &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches)
   ) {
-    document.documentElement.classList.add('dark');
+    document.documentElement.classList.add("dark");
   } else {
-    document.documentElement.classList.remove('dark');
+    document.documentElement.classList.remove("dark");
   }
 };
 
 // 초기 설정
 setDarkMode();
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+
+    <button
+      className="fixed top-0 right-0 w-[100px] h-[100px] cursor-pointer"
+      onClick={() => {
+        window.open("https://bolt.new/");
+      }}
+    >
+      <img src={logo} alt="logo" />
+    </button>
   </StrictMode>
 );
