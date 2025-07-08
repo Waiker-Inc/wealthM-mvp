a2badf16028ff91cb77a5a682f7d95974cbc84aa
noahkang <yongsuek.kang@waiker.ai>
2025-07-08 12:06:52 +0900

    fix: extend 호출 오류 수정

--- src/components/layout/Contents.tsx
+++ src/components/layout/Contents.tsx
@@ -93,17 +93,24 @@ export default function Contents() {
     enabled: !!taskId && !!userId,
   });
 
-  console.log(chatHistoryMessageList, 555);
+  console.log(chatHistoryMessageList, 777);
+
+  const isDev = import.meta.env.DEV;
 
   const { mutate: questionExtendMutate } = useMutation({
     mutationFn: (question: string) => {
-      return fetch('/p1/ai/query/extend', {
-        method: 'POST',
-        body: JSON.stringify({ question }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
+      return fetch(
+        isDev
+          ? '/p1/ai/query/extend'
+          : import.meta.env.VITE_AI_API_URL + '/p1/ai/query/extend',
+        {
+          method: 'POST',
+          body: JSON.stringify({ question }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
     },
     onSuccess: async (data) => {
       const res = await data.json();
